e89f2e7ee50d6c2ddb654310e780300cbeb2a3f7
Al Nahian <alnahian5344@gmail.com>
2024-12-08 01:09:51 +0600

    section change

--- JS/history.js
+++ JS/history.js
@@ -4,6 +4,8 @@ document.getElementById('btn-history-rmv-class').addEventListener('click', funct
     document.getElementById('btn-history-rmv-class').style.backgroundColor = ('rgb(180, 244, 97)');
     document.getElementById('btn-donation-rmv-class').classList.remove('bg-[#B4F461]');
     document.getElementById('btn-donation-rmv-class').style.backgroundColor='';
+    document.getElementById('history-section').classList.remove('hidden');
+    document.getElementById('card-section').classList.add('hidden');
 
 })
 
@@ -11,5 +13,8 @@ document.getElementById('btn-history-rmv-class').addEventListener('click', funct
 document.getElementById('btn-donation-rmv-class').addEventListener('click',function(){
     document.getElementById('btn-donation-rmv-class').style.backgroundColor=('rgb(180, 244, 97)');
     document.getElementById('btn-history-rmv-class').style.backgroundColor='';
+    document.getElementById('card-section').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+
 })
 
