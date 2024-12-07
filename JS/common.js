2bd401825dc562a98d628bd80c76d4ed96a2fc02
Al Nahian <alnahian5344@gmail.com>
2024-12-08 02:23:52 +0600

    blog

--- JS/common.js
+++ JS/common.js
@@ -8,3 +8,16 @@ function getStateInputId(id) {
     const sateInputValue = document.getElementById(id).innerText;
     return sateInputValue;
 }
+
+
+document.getElementById('btn-blog-home').addEventListener('click',function(){
+    window.location.href = '/blog.html';
+})
+
+
+
+document.getElementById('btn-blog-blog').addEventListener('click', function() {
+    window.location.href = '/index.html';
+});
+
+
