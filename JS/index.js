39378e21bfe79cb01d5be76eadf140727bb42873
Al Nahian <alnahian5344@gmail.com>
2024-12-08 22:07:17 +0600

    History update sectin setup

--- JS/index.js
+++ JS/index.js
@@ -8,4 +8,17 @@ document.getElementById('btn-donate-now').addEventListener('click', function () 
     const navStateTotalUpValue = navStateUpdateValueAll - inputValueNoa;
     document.getElementById('sate-value-flood-noa').innerText = sateUpTotalValue.toFixed(2);
     document.getElementById('nav-state-value').innerText = navStateTotalUpValue.toFixed(2);
+
+    const date = new Date();
+    const historyTextForShow = document.getElementById('hist-text-donate-noa').innerText;
+    const historySection = document.getElementById('hist-section');
+    const historyDiv = document.createElement('div');
+    historyDiv.classList.add('mt-4', 'p-2', 'border', 'border-gray-300', 'rounded');
+
+    historyDiv.innerHTML = `
+        <p>${inputValueNoa.toFixed(2)} Taka is, ${historyTextForShow}</p>
+        <p>Date : ${date}</p> `;
+
+    historySection.appendChild(historyDiv);
+    document.getElementById('input-donate-value-noa').value = '';
 })
