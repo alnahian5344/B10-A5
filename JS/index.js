01e16fa2fb283111616c23ad1ce27a8979462be2
Al Nahian <alnahian5344@gmail.com>
2024-12-08 22:31:07 +0600

    validation and success aletrt

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,10 @@ document.getElementById('btn-donate-now').addEventListener('click', function () 
     const inputValueNoa = parseFloat(getInputID('input-donate-value-noa'));
     const statUpdateValueNoa = parseFloat(getStateInputId('sate-value-flood-noa'));
     const navStateUpdateValueAll = parseFloat(getStateInputId('nav-state-value'));
+    if (isNaN(inputValueNoa) || inputValueNoa <= 0) {
+        alert("Please enter a valid donation amount.");
+        return;
+    }
     const sateUpTotalValue = inputValueNoa + statUpdateValueNoa;
     const navStateTotalUpValue = navStateUpdateValueAll - inputValueNoa;
     document.getElementById('sate-value-flood-noa').innerText = sateUpTotalValue.toFixed(2);
@@ -13,7 +17,7 @@ document.getElementById('btn-donate-now').addEventListener('click', function () 
     const historyTextForShow = document.getElementById('hist-text-donate-noa').innerText;
     const historySection = document.getElementById('hist-section');
     const historyDiv = document.createElement('div');
-    historyDiv.classList.add('mt-4', 'p-2', 'border', 'border-gray-300', 'rounded');
+    historyDiv.classList.add('mt-4', 'p-2', 'border', 'border-gray-300', 'rounded','font-bold','text-black');
 
     historyDiv.innerHTML = `
         <p>${inputValueNoa.toFixed(2)} Taka is, ${historyTextForShow}</p>
@@ -21,4 +25,6 @@ document.getElementById('btn-donate-now').addEventListener('click', function () 
 
     historySection.appendChild(historyDiv);
     document.getElementById('input-donate-value-noa').value = '';
+
+    alert("Thank you for your generous donation!");
 })
